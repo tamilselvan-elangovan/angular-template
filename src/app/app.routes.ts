fdac8ed0ac279bc249c30fad6d51337f83ae7225
Tamil Selvan Elangovan <tamilselvan.e@quadkast.co.in>
2024-08-01 11:29:08 +0530

    theming and routing

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,12 @@
 import { Routes } from '@angular/router';
+import { LoginComponent } from './login/login.component';
+import { SignupComponent } from './signup/signup.component';
+import { LayoutComponent } from './layout/layout.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    { path: "", redirectTo: "signin", pathMatch: "full" },
+    { path: "signin", pathMatch: "full", component: LoginComponent },
+    { path: "signup", pathMatch: "full", component: SignupComponent },
+    { path: "dashboard", pathMatch: "full", component: LayoutComponent },
+    { path: "**", redirectTo: "signin", pathMatch: "full" },
+];
